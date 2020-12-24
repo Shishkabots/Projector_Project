afd4c4b4d5db744e804ad31ea00a7b055ed2da3d
Samarths217 <76454836+Samarths217@users.noreply.github.com>
2020-12-23 21:58:49 -0800

    Add files via upload

--- Projector_Project.py
+++ Projector_Project.py
@@ -5,8 +5,12 @@ import time #might be helpful
 
 
 totalDims = 5
-squareWidth = 2 # this needs to change -> corresponds to the x
-squareHeight = 2 # this needs to change -> corresponds to the y
+squareWidth =  1800 # this needs to change -> corresponds to the x
+squareHeight = 1500 # this needs to change -> corresponds to the y
+lswidth = squareWidth/3
+lsheight = squareHeight/3
+lswidth2 = 2 * squareWidth/3
+lsheight2 = 2 * squareHeight/3
 # distanceReadings = [5.65, 4.12, 5.94, 4.2] #feet or meters
 # angleReadings = [43.2, 41.4, 53.5, 48.5] #radians or degrees
 
@@ -28,8 +32,8 @@ def readFileData(file):
     for line in all_lines:
         nums = line.strip().split(' ')
         ang = int(float(nums[0]))
-        if ang > 270:
-            angleReadings.append(ang % 270)
+        if ang >90 and ang < 180:
+            angleReadings.append(ang - 90)
             distanceReadings.append(nums[1])
     print(len(angleReadings))
     print(len(distanceReadings))
@@ -39,19 +43,38 @@ def readFileData(file):
 def convertToXY(angleReadings, distanceReadings):
     xSum = 0
     ySum = 0
+    xDist = []
+    yDist = []
     xDistances = [0.0] * len(distanceReadings)
     yDistances = [0.0] * len(distanceReadings)
     
     for indx in range(len(distanceReadings)):
         xDistances[indx] = float(distanceReadings[indx]) * math.cos(math.radians(angleReadings[indx]))
         yDistances[indx] = float(distanceReadings[indx]) * math.sin(math.radians(angleReadings[indx]))
+        if xDistances[indx] < squareWidth and yDistances[indx] < squareHeight:
+            xDist.append(xDistances[indx])
+            yDist.append(yDistances[indx])
+            if (xDistances[indx] < lswidth and yDistances[indx] < lsheight) and (xDistances[indx] > 0 and yDistances[indx] >0)  :
+                 print ("inside live zone")
+                 print (xDistances[indx])
+                 print (yDistances[indx])
+            if (xDistances[indx] > lswidth or yDistances[indx] > lsheight) and (xDistances[indx] < lswidth2 or yDistances[indx] < lsheight2) :
+                 print ("inside deadzone")
+                 print (xDistances[indx])
+                 print (yDistances[indx])
+            if (xDistances[indx] > lswidth2 and yDistances[indx] > lsheight2) and (xDistances[indx] < squareWidth and yDistances[indx] < squareHeight) :
+                print ("inside live zone 2")
+                print (xDistances[indx])
+                print (yDistances[indx])
 
-    for entry in range(len(distanceReadings)):
-        xSum += xDistances[entry]
-        ySum += yDistances[entry]
+            
+    # for entry in range(len(distanceReadings)):
+    #     xSum += xDistances[entry]
+    #     ySum += yDistances[entry]
         #print("x distance: %f :: y distance: %f" % (xDistances[entry], yDistances[entry]))
-
-    return (xSum, ySum, xDistances, yDistances)
+    print (*xDist, sep = ", ")
+    print (*yDist, sep = ", ")
+    return (xDist, yDist)
 
 ####Assuming the person is represented as a point####
 def finalLocalization(xSum, ySum, xDistances, yDistances):
@@ -68,9 +91,10 @@ def finalLocalization(xSum, ySum, xDistances, yDistances):
 
 
 
-angleReadings, distanceReadings = readFileData('sample_data.txt')
+angleReadings, distanceReadings = readFileData('livetest4.txt')
 results = convertToXY(angleReadings, distanceReadings)
-finalLocalization(results[0], results[1], results[2], results[3])
+#finalLocalization(results[0], results[1], results[2], results[3])
+
 
 
 
