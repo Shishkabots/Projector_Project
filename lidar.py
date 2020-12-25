c01b1b3f8aad2a460942f6371125ebd8404fcbcc
drdonv <50760474+drdonv@users.noreply.github.com>
2020-12-24 21:16:53 -0800

    Update lidar.py

--- lidar.py
+++ lidar.py
@@ -2,22 +2,20 @@ import rplidar
 from rplidar import RPLidar
 import time
 
-#import rospy
-#from rplidar import laser_filters
-lidar = RPLidar('COM4', baudrate=115200, timeout=1, logger=None)
+
+lidar = RPLidar('/dev/ttyUSB0')
 lidar.connect()
-#RPLidar('/dev/ttyUSB0')
-#lidar.stop_motor()
-#time.sleep(3)
-lidar.set_pwm(1023)
+#RPLidar('COM4', baudrate=115200, timeout=1, logger=None)
+
 lidar.start_motor()
+
+#lidar.set_pwm(1023)
 #info = lidar.get_info()
 #print(info)
 
 health = lidar.get_health()
 print(health)
-#angle = lidar
-#print(angle)
+
 
 for i, scan in enumerate(lidar.iter_scans()):
     for counter in range(len(scan)):
